fa1586f2d1c979bed48c0f1f7394fc3543de7d75
- <=>
2022-04-14 14:15:21 +0700

    first commit

--- src/com/kenIT/Main.java
+++ src/com/kenIT/Main.java
@@ -1,7 +1,18 @@
 package com.kenIT;
 
 public class Main {
+    public static int DISTANCE = 100;
+
+    public static int STEP = 2;
 
     public static void main(String[] args) {
+        Car carA = new Car("A");
+        Car carB = new Car("B");
+
+        Thread thread1 = new Thread(carA);
+        Thread thread2 = new Thread(carB);
+        System.out.println("Distance: 100KM");
+        thread1.start();
+        thread2.start();
     }
 }
